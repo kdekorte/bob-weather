173403a5e94d0a3e4bea6ea774b1c22cf58c8b9a
Kevin DeKorte <kdekorte@gmail.com>
2026-09-17 12:35:02 -0600

    Update formula for 1.0.1

--- Casks/bob-weather.rb
+++ Casks/bob-weather.rb
@@ -1,6 +1,6 @@
 cask "bob-weather" do
-  version "1.0.0"
-  sha256 "b89b49376c11f1c854d4078e6994c4f33c087a48d64c8b22464cc16357f15042"
+  version "1.0.1"
+  sha256 "4fb76255cf01fc9ab7325556b6256db155734dfb252d3aa63f4a33f8e8147cd7"
 
   url "https://github.com/kdekorte/bob-weather/releases/download/v#{version}/bob-weather-macos-arm64-#{version}.tar.gz"
   name "bob-weather"
