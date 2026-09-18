c1709e0ac017cf9c577bf6d3d72caa1c30941962
Kevin DeKorte <kdekorte@gmail.com>
2026-09-18 09:54:11 -0600

    Update formula for 1.0.3 (fix tarball bundle name)

--- Casks/weather-dashboard.rb
+++ Casks/weather-dashboard.rb
@@ -1,6 +1,6 @@
 cask "weather-dashboard" do
   version "1.0.3"
-  sha256 "5745e2195e0aaa40ac0388c2113d848687db88dc18b50af42f7cd521cbe72beb"
+  sha256 "2c772b8165cb19e35774a690b0bfa5e1af606caa175e8d4edfdadc88fdf27689"
 
   url "https://github.com/kdekorte/weather-dashboard/releases/download/v#{version}/weather-dashboard-macos-arm64-#{version}.tar.gz"
   name "Weather Dashboard"
